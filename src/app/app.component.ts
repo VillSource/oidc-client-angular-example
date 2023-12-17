a9a1cd0dce2f6f17ef32bf196f653aeca4fd2597
VillSource <anirut1800@gmail.com>
2023-12-17 22:45:53 +0700

    add example how to use oauth code flow PKCE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { OAuthService } from 'angular-oauth2-oidc';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'oidc-client';
+export class AppComponent implements OnInit{
+
+  me = this.httpClient.get("https://localhost:4434/me");
+
+  constructor(private oauthService: OAuthService, private httpClient:HttpClient) {}
+
+  ngOnInit(): void {
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+  }
+
+  login(){
+    // this.oauthService.initCodeFlow();
+    this.oauthService.loadDiscoveryDocumentAndLogin();
+  }
+
+  a(){
+    this.oauthService.loadUserProfile().then(u=>{
+      console.log(u);
+    })
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,52 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthConfig, OAuthModule, OAuthService, OAuthStorage } from 'angular-oauth2-oidc';
+
+const authCodeFlowConfig: AuthConfig = {
+  issuer: 'https://localhost:4434/',
+
+  redirectUri: window.location.origin,
+  clientId: 'console',
+  responseType: 'code',
+  scope: ' ', // 'openid profile email offline_access api',
+  showDebugInformation: true,
+
+};
+
+function storageFactory() : OAuthStorage{
+  try {
+    return localStorage;
+  } catch {
+    return sessionStorage;
+  }
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    OAuthModule.forRoot({
+      resourceServer: {
+        allowedUrls : [
+          "https://localhost:4434/me"
+        ],
+        sendAccessToken: true
+      }
+    }),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {provide: OAuthStorage, useFactory: storageFactory}
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private oauthService: OAuthService) {
+    this.oauthService.configure(authCodeFlowConfig);
+  }
+}
